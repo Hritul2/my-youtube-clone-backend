79eacaccdcbfb9d28c35a05568f48a0e1933231b
Hritul Srivastava <hritul.srivastava@gmail.com>
2024-04-02 11:20:03 +0530

    util code, express middleware setup and asyncHandler

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config();
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(`MongoDB connection Fail: ${error}`);
+    });
 /*
 import express from "express";
 const app = express();
